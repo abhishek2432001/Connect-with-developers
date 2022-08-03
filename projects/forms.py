453bb897affaaf7c814898900d083bc8d48ea003
abhishek <abhishek2432001@gmail.com>
2022-08-03 13:51:26 +0530

    First Commit

---
+++ projects/forms.py
@@ -0,0 +1,22 @@
+from dataclasses import field
+from django import forms
+from django.forms import ModelForm
+from django import forms
+from .models import Project
+
+
+class ProjectForm(ModelForm):
+    class Meta:
+        model = Project
+        fields = ['title','description','featured_image','demo_link','source_link','tags']
+        widgets = {
+            'tags':forms.CheckboxSelectMultiple(),
+        }
+
+        def __init__(self, *args, **kwargs):
+            super(ProjectForm,self).__init__(*args, **kwargs)
+
+            for name,field in self.fields.items():
+                field.widget.attrs.update({'class':'input'})
+
+        
---
+++ users/views.py
@@ -0,0 +1,18 @@
+from multiprocessing import context
+import profile
+from django.shortcuts import render
+from .models import Profile
+
+# Create your views here.
+
+def profiles(request):
+    profiles = Profile.objects.all()
+    context = {'profiles':profiles}
+    return render(request, 'users/profiles.html',context)
+
+
+
+def userProfile (request, pk):
+    profile = Profile.objects.get(id=pk)
+    context = {'profile':profile}
+    return render(request, 'users/user-profile.html',context)
